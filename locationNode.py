30d7cf3c1da06ab680fbddfb351292a7623701bc
jarradnegherbon <50556247+jarradnegherbon@users.noreply.github.com>
2019-05-20 20:18:25 -0700

    Add files via upload

--- locationNode.py
+++ locationNode.py
@@ -38,6 +38,10 @@ class locationNode:
         else:
             self.timeWest = 0
     
+    # this function sets the speed limit at the node
+    def setSpeedLimit(self, speedLimit):
+        self.speedLimit = speedLimit
+    
     # this function sets the weather conditions at the node
     def setWeather(self, weather):
         self.weather = weather
@@ -57,6 +61,39 @@ class locationNode:
     # this function sets if it is a holiday
     def setHoliday(self, holiday):
         self.holiday = holiday
+    
+    # this function sets if a node has been closed
+    def setNodeClosure(self, closure):
+        self.closure = closure
+    
+    # functions to set the distance between nodes
+    def setNorthDistance(self, distance):
+        self.distanceNorth = distance
+    
+    def setSouthDistance(self, distance):
+        self.distanceSouth = distance
+    
+    def setEastDistance(self, distance):
+        self.distanceEast = distance
+    
+    def setWestDistance(self, distance):
+        self.distanceWest = distance
+    
+    def setNorthNeighbor(self, neighbor):
+        self.northNeighbor = neighbor
+    
+    def setSouthNeighbor(self, neighbor):
+        self.southNeighbor = neighbor
+    
+    def setEastNeighbor(self, neighbor):
+        self.eastNeighbor = neighbor
+    
+    def setWestNeighbor(self, neighbor):
+        self.westNeighbor = neighbor
+    
+    # this function gets the name of the node
+    def getNodeName(self):
+        return self.name
     
     # this function returns the distance it takes to travel between nodes
     def getDistance(self, neighbor):
@@ -92,4 +129,37 @@ class locationNode:
         # distance is miles
         time = int(distance) / int(speed)
         return int(time * 60)
-        
+    
+    # this function gets the northern neighbor
+    def getNorthNeighbor(self):
+        return self.northNeighbor
+    
+    # this function gets the southern neighbor
+    def getSouthNeighbor(self):
+        return self.southNeighbor
+    
+    # this function gets the eastern neighbor
+    def getEastNeighbor(self):
+        return self. eastNeighbor
+    
+    # this function gets the western neighbor
+    def getWestNeighbor(self):
+        return self.westNeighbor
+    
+    # this function will get the speed limit at the node
+    def getSpeedLimit(self):
+        return self.speedLimit
+    
+    # this function gets the weather at the node
+    def getWeather(self):
+        return self.weather
+    
+    # this function gets the traffic at the node
+    def getTraffic(self):
+        return self.traffic
+    
+    
+    
+    
+    
+    
--- simulator.py
+++ simulator.py
@@ -1,9 +1,28 @@
+import queue
 from locationNode import *
 from locationInitializor import *
+from helperFunctions import *
 
 # just testing the functions and linking of the python files
 
 print(a1.getDistance('b1'))
+print(b1.getDistance('c1'))
+print(c1.getDistance('c2'))
+print(d1.getDistance('c1'))
 z = a2.getTime('a1')
 
 print('It will take ' + str(z) + ' minutes to get from a2 to a1')
+
+
+start_city = input('Please enter the starting point: ')
+print('The starting city input is: ' + start_city)
+
+q = buildNodeQueue()
+
+while True:
+    if q.empty():
+        break
+    x = q.get()
+    print('The node name is: ' + x.getNodeName())
+
+print('exited while loop')
