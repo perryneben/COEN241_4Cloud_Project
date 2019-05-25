e1bfaffc6f36c369e994ec9a5560f4ab02104e03
perryneben <35615334+perryneben@users.noreply.github.com>
2019-05-25 11:27:27 -0700

    Add in regression model and their r^2
    Looks like the best are the linear models, which does match the data generated (factor I used is linear).

--- machineLearningTrain.py
+++ machineLearningTrain.py
@@ -16,6 +16,19 @@ from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.naive_bayes import GaussianNB
 from sklearn.svm import SVC
 
+from sklearn.svm import SVR
+from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import Lasso
+from sklearn.linear_model import LassoLars
+from sklearn.linear_model import MultiTaskLasso
+from sklearn.linear_model import ElasticNet
+from sklearn.linear_model import MultiTaskElasticNet
+from sklearn.linear_model import OrthogonalMatchingPursuit
+from sklearn.linear_model import BayesianRidge
+from sklearn.linear_model import ARDRegression
+from sklearn.kernel_ridge import KernelRidge
+from sklearn.linear_model import SGDRegressor
+from sklearn.gaussian_process import GaussianProcessRegressor
 if len(sys.argv) < 2:
     print("Must provide csv file of training data")
     sys.exit(1)
@@ -47,13 +60,25 @@ seed = 7
 scoring = 'explained_variance'
 X_train, X_validation, Y_train, Y_validation = model_selection.train_test_split(X, Y, test_size=validation_size, random_state=seed)
 
+print(X_train.shape)
+print(Y_train.shape)
+print(X_validation.shape)
+print(Y_validation.shape)
+
 models = []
-models.append(('LR', LogisticRegression(solver='liblinear', multi_class='ovr')))
-models.append(('LDA', LinearDiscriminantAnalysis()))
-models.append(('KNN', KNeighborsClassifier()))
-models.append(('CART', DecisionTreeClassifier()))
-models.append(('NB', GaussianNB()))
-models.append(('SVM', SVC(gamma='auto')))
+models.append(('LinearRegression', LinearRegression())) # r^2 of 0.774
+models.append(('Lasso', Lasso(alpha=0.1))) # r^2 of 0.773
+models.append(('LassoLars', LassoLars(alpha=0.01))) # r^2 of 0.773
+models.append(('ElasticNet', ElasticNet(random_state=0, alpha=0.1))) # r^2 of 0.727
+models.append(('OrthogonalMatchingPursuit', OrthogonalMatchingPursuit())) # r^2 of 0.349
+models.append(('BayesianRidge', BayesianRidge())) # r^2 of 0.774
+models.append(('KernelRidge', KernelRidge(alpha=1.0))) #r^2 of 0.754
+# models.append(('ARDRegression', ARDRegression())) # r^2 of ??? Doesn't complete.
+#models.append(('SVR', SVR(gamma='auto'))) # r^2 of 0.005
+#models.append(('SGDRegressor_sl', SGDRegressor(max_iter=1000, tol=1e-3, loss="squared_loss"))) # Doesn't work, negative r^2
+models.append(('SGDRegressor_h', SGDRegressor(max_iter=1000, tol=1e-3, loss="huber"))) # r^2 of 0.388
+#models.append(('SGDRegressor_ei', SGDRegressor(max_iter=1000, tol=1e-3, loss="epsilon_insensitive"))) # Doesn't work, negative r^2
+#models.append(('GaussianProcessRegressor', GaussianProcessRegressor())) # r^2 of 0.003
 
 results = []
 names = []
@@ -62,5 +87,6 @@ for name, model in models:
 	cv_results = model_selection.cross_val_score(model, X_train, Y_train, cv=kfold, scoring=scoring)
 	results.append(cv_results)
 	names.append(name)
-	msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
+	msg = "%s r^2: %f (%f)" % (name, cv_results.mean(), cv_results.std())
 	print(msg)
+	print(cv_results)
